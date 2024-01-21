d3774133fccb7841e82318515dbc1455fa11311a
Ulukman Zhyrgalbek uulu <uluk001@MacBook-Pro-2.local>
2024-01-21 13:17:31 +0600

    Add DELETE, GET and UPDATE endpoints for product reviews

--- routes/product_review.py
+++ routes/product_review.py
@@ -10,6 +10,9 @@ from services.product_review.product_review_schema import (
 from services.product_review.product_review_crud import (
     create_product_review,
     get_all_product_reviews,
+    get_product_review_by_id,
+    update_product_review,
+    delete_product_review,
 )
 
 
@@ -24,3 +27,29 @@ async def create_product_review_route(product_review: ProductReviewCreate) -> Pr
 @router.get("/", response_model=List[ProductReviewRead])
 async def get_all_product_reviews_route() -> List[ProductReviewRead]:
     return await get_all_product_reviews()
+
+
+@router.get("/{id}", response_model=ProductReviewRead)
+async def get_product_review_by_id_route(id: PydanticObjectId) -> ProductReviewRead:
+    product_review = await get_product_review_by_id(id)
+    if not product_review:
+        raise HTTPException(status_code=404, detail="Product Review not found")
+    return product_review
+
+
+@router.put("/{id}", response_model=ProductReviewRead)
+async def update_product_review_route(
+    id: PydanticObjectId, product_review_data: ProductReviewUpdate
+) -> ProductReviewRead:
+    product_review = await update_product_review(id, product_review_data)
+    if not product_review:
+        raise HTTPException(status_code=404, detail="Product Review not found")
+    return product_review
+
+
+@router.delete("/{id}", response_model=ProductReviewRead)
+async def delete_product_review_route(id: PydanticObjectId) -> ProductReviewRead:
+    product_review = await delete_product_review(id)
+    if not product_review:
+        raise HTTPException(status_code=404, detail="Product Review not found")
+    return product_review
--- services/product_review/product_review_crud.py
+++ services/product_review/product_review_crud.py
@@ -18,3 +18,22 @@ async def create_product_review(product_review: ProductReviewCreate) -> ProductR
     product_review = ProductReview(**product_review.model_dump())
     await product_review.insert()
     return product_review
+
+
+async def update_product_review(
+    id: str, product_review_data: ProductReviewUpdate
+) -> ProductReview:
+    product_review = await ProductReview.get(id)
+
+    # Update fields in product_review with values from product_review_data
+    for field, value in product_review_data.dict().items():
+        setattr(product_review, field, value)
+
+    await product_review.save()
+    return product_review
+
+
+async def delete_product_review(id: str) -> ProductReview:
+    product_review = await ProductReview.get(id)
+    await product_review.delete()
+    return product_review
